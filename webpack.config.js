48ceb2bba23d46b83ce25ba28ad877e00d63a248
Basiltoast <basiltoast@gmail.com>
2019-11-12 20:08:49 +0900

    chore : esLint, prettier ignore파일 설정

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,42 @@ const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const smp = new SpeedMeasurePlugin();
 
 module.exports = smp.wrap({
-    devtool: 'cheap-module-eval-source-map',
-    mode: "development",
-    entry : __dirname + '/src/index.js',
-    output : {
-        path: __dirname + '/public',
-        filename : 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude : /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(css)$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx', '.css']
-    },
-    plugins: [
-
-    ],
-    devServer: {
-        clientLogLevel: 'silent',
-        disableHostCheck: true,
-        contentBase: path.join(__dirname, 'public'),
-        compress: false,
-        inline: true,
-        hot: true,
-        liveReload: false,
-        watchContentBase: false,
-        overlay: {
-            errors : true
-        },
-        port: 3000
-    }
-})
+	devtool: 'cheap-module-eval-source-map',
+	mode: 'development',
+	entry: __dirname + '/src/index.js',
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.(css)$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.css']
+	},
+	plugins: [],
+	devServer: {
+		clientLogLevel: 'silent',
+		disableHostCheck: true,
+		contentBase: path.join(__dirname, 'public'),
+		compress: false,
+		inline: true,
+		hot: true,
+		liveReload: false,
+		watchContentBase: false,
+		overlay: {
+			errors: true
+		},
+		port: 3000
+	}
+});
