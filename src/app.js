0e3d0b6496d22ebda5726a0ddb7d245d5544ae97
Basiltoast <basiltoast@gmail.com>
2019-11-12 20:03:11 +0900

    chore : prettier 설정 완료

--- src/app.js
+++ src/app.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 function App() {
-    if ('test'==='test'){
-        console.log('test');
-    } else {
-        
-    }
+	if ('test' === 'test') console.log('test');
+	else console.log('test');
 
-    
-    const obj = {
-        t: 'test'
-    };
-    
-    return (
-        <div>
-            <header>
-                <h1>Hello! Cocode!!</h1>
-            </header>
-            <section>
-
-            </section>
-            <footer>
-
-            </footer>
-        </div>
-    );
+	return (
+		<div>
+			<header>
+				<h1>Hello! Cocode!!</h1>
+			</header>
+			<section></section>
+			<footer></footer>
+		</div>
+	);
 }
 
 export default App;
